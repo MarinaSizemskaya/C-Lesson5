e1a58bef39b7256dec0d5f7f3e95b6d7e2284e59
Marina <sizemskaya@gmail.com>
2022-11-26 20:54:40 +0300

    Задача 3 дополнительная

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Задайте массив из 8 случайных чисел. 
+// Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+using System;
+using static System.Console;
+Clear();
+
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    array[i]=new Random().Next(0, 100);
+    Write(array[i] + " ");
+}
+WriteLine();
+Write("Введите число: ");
+int A = int.Parse(ReadLine());
+int count = 0;
+for (int i = 0; i < 8; i++)
+{
+    if (array[i]==A) 
+    {
+        count++;
+    }
+}
+if (count > 0) WriteLine("Такое число присутствует в массиве");
+else WriteLine("Такого числа нет в массиве");
